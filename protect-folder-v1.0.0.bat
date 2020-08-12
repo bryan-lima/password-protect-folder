8d9b0c3f3d1a3d6788f8ec67292ccdc18d39974a
Bryan <bryanlima200@outlook.com>
2020-08-12 19:27:41 -0300

    Add protect-folder-v1.0.0.bat

---
+++ protect-folder-v1.0.0.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+REM Proteger pasta com senha - https://github.com/bryan-lima/scripts-batch/password-protect-folder
+REM Proteja sua pasta com uma senha local
+REM
+REM Desenvolvido por BRYAN LIMA
+REM Copyright© 2020
+REM Versão: 1.0.0
+REM
+REM Github: @bryan-lima
+REM 
+REM Se te ajudei de alguma forma, considere me pagar um café como agradecimento
+REM https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6XRSGDU6FKWT2&source=url
+REM ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
