0fb81dc85ae9c2a38fcbb3ab11d82ee6d9734289
Bryan <bryanlima200@outlook.com>
2020-08-12 22:53:59 -0300

    Alterado encoding de ANSI para UTF-8

--- protect-folder-v1.0.0.bat
+++ protect-folder-v1.0.0.bat
@@ -1,15 +1,16 @@
 @echo off
 
 REM ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-REM Proteger pasta com senha - https://github.com/bryan-lima/scripts-batch/password-protect-folder
+REM Proteger pasta com senha - https://github.com/bryan-lima/password-protect-folder
 REM Proteja sua pasta com uma senha local
 REM
 REM Desenvolvido por BRYAN LIMA
-REM Copyright© 2020
-REM Versão: 1.0.0
+REM CopyrightÂ© 2020
+REM VersÃ£o: 1.0.0
 REM
 REM Github: @bryan-lima
 REM 
-REM Se te ajudei de alguma forma, considere me pagar um café como agradecimento
+REM Se te ajudei de alguma forma, considere me pagar um cafÃ© como agradecimento
 REM https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6XRSGDU6FKWT2&source=url
 REM ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
