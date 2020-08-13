86363178d86b0b379601107f23265649fda93a9d
Bryan <bryanlima200@outlook.com>
2020-08-12 22:56:54 -0300

    Add menu

--- protect-folder-v1.0.0.bat
+++ protect-folder-v1.0.0.bat
@@ -14,3 +14,37 @@ REM Se te ajudei de alguma forma, considere me pagar um café como agradecimento
 REM https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6XRSGDU6FKWT2&source=url
 REM ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+@chcp 65001 > nul
+
+REM ====================================================
+REM                        MAIN
+REM ====================================================
+:MAIN
+cls
+title PROTEGER PASTA COM SENHA - Menu
+mode 58,18
+color 1f
+echo.
+echo.
+echo              Desenvolvido por BRYAN LIMA.
+echo      Todos os direitos reservados. Copyright© 2020.
+echo                      Versão 1.0.0
+echo.
+echo.
+echo               PROTEGER PASTA COM SENHA
+echo.
+echo.
+echo    [1] Proteger
+echo    [2] Desproteger
+echo    [X] Sair
+echo.
+set /p "op=Escolha o que deseja: "
+
+if %op%==1 goto LOCK
+if %op%==2 goto UNLOCK
+if %op%==X goto EXIT
+if %op%==x goto EXIT
+if %op% NEQ 1 goto INVALID
+if %op% NEQ 2 goto INVALID
+if %op% NEQ X goto INVALID
+if %op% NEQ x goto INVALID
